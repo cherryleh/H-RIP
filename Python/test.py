52c1de406eca372b199c8df44e10d965e7b0e8fe
Cherryle <cherryle@h-rip.js2local>
2024-02-05 18:15:28 +0000

    Daily temp 2/4/24

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,4 @@
 # Create a test.txt file
-with open('test.txt', 'w') as file:
-    file.write('Hello, this is a test file!\n')
-
-print('test.txt file created successfully.')
+import os
+os.chdir('/var/www/html/Python')
+print(os.getcwd())
