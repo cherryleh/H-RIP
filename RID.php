49dc809e0d12eb13187fa7567f4b5ab2475814d6
cherryleh <cherryle@hawaii.edu>
2025-08-20 21:40:28 -1000

    Fix total dry days

--- RID.php
+++ RID.php
@@ -111,7 +111,7 @@
         // Loop through each row of the CSV
         while (($data = fgetcsv($handle)) !== FALSE) {
             // Check if the value in the target column is less than 1
-            if (isset($data[4]) && is_numeric($data[4]) && $data[4] < 0.03937) {
+            if (isset($data[3]) && is_numeric($data[3]) && $data[3] < 0.03937) {
                 $total_dry_days++;
             }
         }
