b120684a8e3d0d51dfd6a4622b06ad24eb806cc8
cherryleh <cherryle@hawaii.edu>
2026-03-02 09:25:32 -1000

    temp dir

--- Python/temp_monthly.py
+++ Python/temp_monthly.py
@@ -9,7 +9,7 @@ import requests
 import calendar
 
 import os
-#os.chdir('/Users/cherryleheu/Codes/NIDIS-Codes/H-RIP/Python')
+# os.chdir('/Users/cherryleheu/Codes/NIDIS-Codes/H-RIP/Python')
 os.chdir('./Python')
 #Get the number of ranches in HRIP
 dir_path = '../RID'
